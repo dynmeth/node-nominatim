913f8f1b0df652268f79790f17f7cecfd45283a3
Francisco Méndez Vilas <fmvilas@gmail.com>
2015-10-06 16:20:52 +0200

    Fixed OSM Nominatim policy requirement about UA header, and tests

--- test/reverse.test.js
+++ test/reverse.test.js
@@ -1,9 +1,9 @@
 var nominatim = require('..');
 
-nominatim.search({ q: 'Adelaide, 5000, South Australia, Australia'}, function(err, results) {
-  var item = results.data[0];
+nominatim.search({ q: 'Adelaide, 5000, South Australia, Australia'}, function(err, opts, results) {
+  var item = results[0];
   console.log(results);
-  nominatim.reverse({ lat: item.lat, lon: item.lon }, function(err, results) {
+  nominatim.reverse({ lat: item.lat, lon: item.lon }, function(err, opts, results) {
   	console.log(results);
   });
 });
